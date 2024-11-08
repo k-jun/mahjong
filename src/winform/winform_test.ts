b1b99e3a5d6b7eb7f457f941c1cbf9298862d603
k-jun <keijun091221@gmail.com>
2024-11-09 04:29:24 +0900

    delete(winform): debug code

--- src/winform/winform_test.ts
+++ src/winform/winform_test.ts
@@ -5,12 +5,6 @@ import { NewWinForm } from "./winform.ts";
 Deno.test("NewWinForm", async () => {
   await fixtures((params) => {
     const wins = NewWinForm({ ...params });
-    if (wins.length == 0) {
-      console.log(params);
-    }
-    if (wins.length > 1) {
-      console.log(wins.map((e) => e.paiSets.map((e) => e.pais.map((e) => e.val))));
-    }
     expect(wins.length != 0).toBe(true);
   }, 10000);
 });
