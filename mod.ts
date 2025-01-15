789ef1c3b0526f7e81097fc52b85032ebefcc966
k-jun <keijun091221@gmail.com>
2025-01-15 23:53:55 +0900

    feat: update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import { Shanten } from "./src/shanten/shanten.ts";
 import { Tokuten } from "./src/tokuten/tokuten.ts";
-import { Pai, PaiType } from "./src/pai/pai.ts";
-import { PaiSet, PaiSetType } from "./src/paiset/paiset.ts";
+import { Pai } from "./src/pai/pai.ts";
+import { PaiSet, PaiSetType, Player } from "./src/paiset/paiset.ts";
 export { Shanten } from "./src/shanten/shanten.ts";
 export { Tokuten } from "./src/tokuten/tokuten.ts";
 export { Pai, PaiType } from "./src/pai/pai.ts";
@@ -10,12 +10,12 @@ export { PaiSet, PaiSetType } from "./src/paiset/paiset.ts";
 const tokuten = new Tokuten({
   options: {
     isOya: true,
-    isTsumo: false,
+    isTsumo: true,
   },
   paiBakaze: new Pai("z1"),
   paiJikaze: new Pai("z1"),
-  paiDora: [new Pai("m1")],
-  paiDoraUra: [new Pai("m1")],
+  paiDora: [new Pai("z2")],
+  paiDoraUra: [new Pai("z2")],
   paiRest: [
     new Pai("m1"),
     new Pai("m1"),
@@ -25,14 +25,24 @@ const tokuten = new Tokuten({
     new Pai("m3"),
     new Pai("m3"),
     new Pai("m3"),
-    new Pai("m4"),
-    new Pai("m4"),
-    new Pai("m4"),
-    new Pai("m5"),
-    new Pai("m5"),
+    new Pai("z6"),
+    new Pai("z6"),
   ],
   paiLast: new Pai("m1"),
-  paiSets: [],
+  paiSets: [
+    new PaiSet(
+      {
+        pais: [
+          new Pai("m4"),
+          new Pai("m4"),
+          new Pai("m4"),
+        ],
+        nakiIdx: 1,
+        type: PaiSetType.MINKO,
+        fromWho: Player.SHIMOCHA,
+      },
+    ),
+  ],
 });
 console.log(tokuten.count());
 
