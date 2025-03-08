f53aa5eb24a75e892fa79c342432c07266cabc49
k-jun <keijun091221@gmail.com>
2025-03-08 22:45:44 +0900

    feat: export Player too

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 export { Shanten } from "./src/shanten/shanten.ts";
 export { Tokuten } from "./src/tokuten/tokuten.ts";
 export { Pai, PaiType } from "./src/pai/pai.ts";
-export { PaiSet, PaiSetType } from "./src/paiset/paiset.ts";
+export { PaiSet, PaiSetType, Player } from "./src/paiset/paiset.ts";
